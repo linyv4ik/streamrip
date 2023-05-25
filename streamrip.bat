257917718586ec18bdb6a182d1799af1e0abb355
linyv4ik <134552208+linyv4ik@users.noreply.github.com>
2023-05-26 00:18:53 +0300

    Update streamrip.bat

--- streamrip.bat
+++ streamrip.bat
@@ -1,53 +1,45 @@
-::v1.01
+::v1.02
 chcp 65001
 @echo off
+if not exist "C:\ProgramData\streamrip" mkdir "C:\ProgramData\streamrip"
+if not exist "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip" mkdir "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip"
 cls
 :loop
 set /P input="Введіть посилання на альбом Deezer: "
 
 ::canada
-del /f/q "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml"
-if not exist "C:\ProgramData\TEMP\streamrip" mkdir "C:\ProgramData\TEMP\streamrip"
-xcopy "C:\Users\%USERNAME%\AppData\Local\streamrip\canada.toml" "C:\ProgramData\TEMP\streamrip" /y
-rename "C:\ProgramData\TEMP\streamrip\canada.toml" config.toml
-move "C:\ProgramData\TEMP\streamrip\config.toml" "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip"
-if exist "C:\ProgramData\TEMP\streamrip" rmdir /S /Q "C:\ProgramData\TEMP\streamrip"
+if exist "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml" del /f/q "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml"
+xcopy "%cd%\streamrip\canada.toml" "C:\ProgramData\streamrip" /y
+rename "C:\ProgramData\streamrip\canada.toml" config.toml
+move "C:\ProgramData\streamrip\config.toml" "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip"
 rip url --max-quality 4 %input%
 
 ::france
-del /f/q "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml"
-if not exist "C:\ProgramData\TEMP\streamrip" mkdir "C:\ProgramData\TEMP\streamrip"
-xcopy "C:\Users\%USERNAME%\AppData\Local\streamrip\france.toml" "C:\ProgramData\TEMP\streamrip" /y
-rename "C:\ProgramData\TEMP\streamrip\france.toml" config.toml
-move "C:\ProgramData\TEMP\streamrip\config.toml" "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip"
-if exist "C:\ProgramData\TEMP\streamrip" rmdir /S /Q "C:\ProgramData\TEMP\streamrip"
+if exist "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml" del /f/q "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml"
+xcopy "%cd%\streamrip\france.toml" "C:\ProgramData\streamrip" /y
+rename "C:\ProgramData\streamrip\france.toml" config.toml
+move "C:\ProgramData\streamrip\config.toml" "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip"
 rip url --max-quality 4 %input%
 
 ::mauritius
-del /f/q "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml"
-if not exist "C:\ProgramData\TEMP\streamrip" mkdir "C:\ProgramData\TEMP\streamrip"
-xcopy "C:\Users\%USERNAME%\AppData\Local\streamrip\mauritius.toml" "C:\ProgramData\TEMP\streamrip" /y
-rename "C:\ProgramData\TEMP\streamrip\mauritius.toml" config.toml
-move "C:\ProgramData\TEMP\streamrip\config.toml" "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip"
-if exist "C:\ProgramData\TEMP\streamrip" rmdir /S /Q "C:\ProgramData\TEMP\streamrip"
+if exist "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml" del /f/q "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml"
+xcopy "%cd%\streamrip\mauritius.toml" "C:\ProgramData\streamrip" /y
+rename "C:\ProgramData\streamrip\mauritius.toml" config.toml
+move "C:\ProgramData\streamrip\config.toml" "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip"
 rip url --max-quality 4 %input%
 
 ::peru
-del /f/q "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml"
-if not exist "C:\ProgramData\TEMP\streamrip" mkdir "C:\ProgramData\TEMP\streamrip"
-xcopy "C:\Users\%USERNAME%\AppData\Local\streamrip\peru.toml" "C:\ProgramData\TEMP\streamrip" /y
-rename "C:\ProgramData\TEMP\streamrip\peru.toml" config.toml
-move "C:\ProgramData\TEMP\streamrip\config.toml" "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip"
-if exist "C:\ProgramData\TEMP\streamrip" rmdir /S /Q "C:\ProgramData\TEMP\streamrip"
+if exist "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml" del /f/q "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml"
+xcopy "%cd%\streamrip\peru.toml" "C:\ProgramData\streamrip" /y
+rename "C:\ProgramData\streamrip\peru.toml" config.toml
+move "C:\ProgramData\streamrip\config.toml" "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip"
 rip url --max-quality 4 %input%
 
 ::unitedstates
-del /f/q "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml"
-if not exist "C:\ProgramData\TEMP\streamrip" mkdir "C:\ProgramData\TEMP\streamrip"
-xcopy "C:\Users\%USERNAME%\AppData\Local\streamrip\unitedstates.toml" "C:\ProgramData\TEMP\streamrip" /y
-rename "C:\ProgramData\TEMP\streamrip\unitedstates.toml" config.toml
-move "C:\ProgramData\TEMP\streamrip\config.toml" "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip"
-if exist "C:\ProgramData\TEMP\streamrip" rmdir /S /Q "C:\ProgramData\TEMP\streamrip"
+if exist "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml" del /f/q "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml"
+xcopy "%cd%\streamrip\unitedstates.toml" "C:\ProgramData\streamrip" /y
+rename "C:\ProgramData\streamrip\unitedstates.toml" config.toml
+move "C:\ProgramData\streamrip\config.toml" "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip"
 rip url --max-quality 4 %input%
 
 goto loop
