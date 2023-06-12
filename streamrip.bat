5f4705723cff0d879bcb9b5f51739f0902ab61b2
linyv4ik <134552208+linyv4ik@users.noreply.github.com>
2023-06-12 13:45:37 +0300

    Update streamrip.bat

--- streamrip.bat
+++ streamrip.bat
@@ -4,12 +4,17 @@ chcp 65001
 if not exist "C:\ProgramData\streamrip" mkdir "C:\ProgramData\streamrip"
 if not exist "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip" mkdir "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip"
 cls
-:loop
+:menu
 echo.
-echo 1 Видалити історію завантаження
+echo Введіть посилання на альбом Deezer або виберіть бажану опцію з меню
 echo.
-set /P input="Введіть посилання на альбом Deezer: "
-if %input% == 1 (goto deezer_db)
+echo 1 Завантажити посилання з файлу url.txt
+echo 2 Видалити історію завантаження
+echo.
+set /P input=""
+if %input% == 1 (goto file_dl)
+if %input% == 2 (goto deezer_db)
+
 
 ::australia
 if exist "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml" del /f/q "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml"
@@ -60,11 +65,64 @@ rename "C:\ProgramData\streamrip\unitedstates.toml" config.toml
 move "C:\ProgramData\streamrip\config.toml" "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip"
 rip url --max-quality 4 %input%
 
-goto loop
+goto menu
+
+:file_dl
+
+::australia
+if exist "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml" del /f/q "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml"
+xcopy "%cd%\streamrip\australia.toml" "C:\ProgramData\streamrip" /y
+rename "C:\ProgramData\streamrip\australia.toml" config.toml
+move "C:\ProgramData\streamrip\config.toml" "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip"
+rip url --max-quality 4 --file url.txt
+
+::canada
+if exist "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml" del /f/q "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml"
+xcopy "%cd%\streamrip\canada.toml" "C:\ProgramData\streamrip" /y
+rename "C:\ProgramData\streamrip\canada.toml" config.toml
+move "C:\ProgramData\streamrip\config.toml" "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip"
+rip url --max-quality 4 --file url.txt
+
+::france
+if exist "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml" del /f/q "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml"
+xcopy "%cd%\streamrip\france.toml" "C:\ProgramData\streamrip" /y
+rename "C:\ProgramData\streamrip\france.toml" config.toml
+move "C:\ProgramData\streamrip\config.toml" "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip"
+rip url --max-quality 4 --file url.txt
+
+::mauritius
+if exist "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml" del /f/q "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml"
+xcopy "%cd%\streamrip\mauritius.toml" "C:\ProgramData\streamrip" /y
+rename "C:\ProgramData\streamrip\mauritius.toml" config.toml
+move "C:\ProgramData\streamrip\config.toml" "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip"
+rip url --max-quality 4 --file url.txt
+
+::peru
+::if exist "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml" del /f/q "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml"
+::xcopy "%cd%\streamrip\peru.toml" "C:\ProgramData\streamrip" /y
+::rename "C:\ProgramData\streamrip\peru.toml" config.toml
+::move "C:\ProgramData\streamrip\config.toml" "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip"
+::rip url --max-quality 4 --file url.txt
+
+::turkey
+if exist "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml" del /f/q "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml"
+xcopy "%cd%\streamrip\turkey.toml" "C:\ProgramData\streamrip" /y
+rename "C:\ProgramData\streamrip\turkey.toml" config.toml
+move "C:\ProgramData\streamrip\config.toml" "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip"
+rip url --max-quality 4 --file url.txt
+
+::unitedstates
+if exist "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml" del /f/q "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip\config.toml"
+xcopy "%cd%\streamrip\unitedstates.toml" "C:\ProgramData\streamrip" /y
+rename "C:\ProgramData\streamrip\unitedstates.toml" config.toml
+move "C:\ProgramData\streamrip\config.toml" "C:\Users\%USERNAME%\AppData\Local\streamrip\streamrip"
+rip url --max-quality 4 --file url.txt
+
+goto menu
 
 :deezer_db
 if exist "C:\ProgramData\streamrip\downloads.db" del /f/q "C:\ProgramData\streamrip\downloads.db"
 if exist "C:\ProgramData\streamrip\failed_downloads.db" del /f/q "C:\ProgramData\streamrip\failed_downloads.db"
 echo.
 echo Історію завантаження видалено!
-goto loop
+goto menu
